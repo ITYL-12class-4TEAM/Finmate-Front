1dd9ded3fa8aff5e86f7ac82bbfc235b5d447505
BaileyChoi <haileychoi9981@gmail.com>
2025-07-16 16:24:21 +0900

    style: 기본 전역 스타일 및 컬러 변수 common.css 설정

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./assets/styles/common.css";
 
-import 'bootstrap';
-import App from './App.vue';
-import router from './router';
+import "bootstrap";
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
