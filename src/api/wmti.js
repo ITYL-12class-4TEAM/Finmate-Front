70a435eaded1567e1ebcc8943b20a41fd57c4ee0
이주영 <ptljoo1572@gmail.com>
2025-07-29 16:48:30 +0900

    feat: wmti 설문문항(질문20개) 가져오기, 설문답변(사용자입력값20개) 제출용 api 정의

--- src/api/wmti.js
+++ src/api/wmti.js
@@ -5,3 +5,14 @@ export const postPreinfoAPI = async (info) => {
   const res = await api.post('/api/preinfo/submit', info);
   return res.data;
 };
+//wmti설문문항 가져오기
+export const getWMTIQuestionsAPI = async () => {
+  const res = await api.get('/api/wmti/questions');
+  return res.data;
+};
+
+// wmti설문답변 제출
+export const postwmtiAPI = async (info) => {
+  const res = await api.post('/api/wmti/submit', info);
+  return res.data;
+};
