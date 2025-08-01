ec2fe17bb516d9a06f9bdffcc982d43e9aeddb0b
BaileyChoi <haileychoi9981@gmail.com>
2025-08-01 16:45:50 +0900

    feat: 전역 모달 시스템 구현 및 useModal 훅 추가
    - ModalProvider에서 provide로 모달 상태 및 핸들러 주입
    - Modal.vue에서 inject로 모달 동작 처리
    - useModal() 훅을 통해 전역 어디서나 모달 호출 가능하도록 구현
    - App.vue에서 ModalProvider로 전체 앱 감싸기 완료

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <LayoutDefault />
-  <ModalProvider />
+  <ModalProvider>
+    <LayoutDefault />
+  </ModalProvider>
 </template>
 
 <script setup>
-import { reactive, provide } from 'vue';
 import LayoutDefault from './layouts/LayoutDefault.vue';
 import ModalProvider from '@/components/common/ModalProvider.vue';
-
-// Modal
-const modalState = reactive({
-  isOpen: false,
-  message: '',
-});
-
-const modalHandlers = {
-  resolve: null,
-};
-
-provide('modalState', modalState);
-provide('modalHandlers', modalHandlers);
 </script>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -2,12 +2,27 @@
   <div class="main">
     <h1>Welcome to Finmate!</h1>
     <p>This is the main page.</p>
+    <button @click="handleDelete">삭제하기</button>
     <router-link to="/this-page-does-not-exist">Go to not found</router-link>
   </div>
 </template>
 
 <script setup>
 // 필요한 경우 script 코드 작성 가능
+import { useModal } from '@/composables/useModal';
+
+const { showModal } = useModal();
+
+const handleDelete = async () => {
+  const confirm = await showModal('정말 삭제하시겠습니까?');
+  if (confirm) {
+    // 확인 시 처리
+    console.log('삭제 실행');
+  } else {
+    // 취소 시 처리
+    console.log('삭제 취소');
+  }
+};
 </script>
 
 <style scoped>
