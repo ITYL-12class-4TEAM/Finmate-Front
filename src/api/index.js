0cc913d03bfff98cf47e67162171e0d5c9260cad
BaileyChoi <haileychoi9981@gmail.com>
2025-08-07 09:52:55 +0900

    feat: .env Git 추적 제외 및 Axios baseURL 환경변수로 설정

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // Axios 인스턴스 생성
 const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
